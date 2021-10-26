31ab5473e4026383f9812454c12bed3148579abe
R.Shafigin <sh_raul08@mail.ru>
2021-10-26 11:03:15 +0200

    Initial Commit

---
+++ loginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginApp
+//
+//  Created by Raul Shafigin on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
